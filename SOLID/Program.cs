c5c7b0cabb1130fac769c0a886bfe02387708a53
TheEnmanuel23 <enmisac@gmail.com>
2018-01-28 00:06:14 -0600

    Proyecto sin SOLID

---
+++ SOLID/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rectangulos = new[] {
+                new Rectangle { Width = 10, Height = 5 },
+                new Rectangle { Width = 4, Height = 6 }
+            };
+
+            var sumaAreas = Rectangle.SumAreas(rectangulos);
+            var sumaPerimetros = Rectangle.SumParemeters(rectangulos);
+
+            Console.WriteLine("Suma de areas: " + sumaAreas);
+            Console.WriteLine("Suma de perimetros: " + sumaPerimetros);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SOLID/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    public class Rectangle
+    {
+        private double slides = 4;
+        public double Sides { 
+            get { return this.slides; } 
+        }
+        public double Height { get; set; }
+        public double Width { get; set; }
+
+        public static double SumAreas(IEnumerable<Rectangle> rectangles)
+        {
+            double area = 0;
+            foreach (var rectangle in rectangles)
+                area += rectangle.Height * rectangle.Width;
+
+            return area;
+        }
+
+        public static double SumParemeters(IEnumerable<Rectangle> rectangles)
+        {
+            double parameter = 0;
+            foreach (var rectangle in rectangles)
+                parameter += 2 * rectangle.Height + 2 * rectangle.Width;
+            return parameter;
+        }
+    }
+}
